38256bc0cf616fc3d2dafb3dfb541e85e847bd52
TOMAS AGUSTIN AVOLA <47297744@ort>
2023-11-02 09:01:38 -0300

    avanzando Ver Detalle

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -9,42 +9,60 @@ export default function ProjectDetail() {
 
   let { dataPortfolio } = useContext(DataPortfolioContext);
   let { favorites, addCreation, deleteCreation } = useContext(FavoritosContext);
-  const { creationId } = useParams();
+  const { id } = useParams();
   const [creation, setCreation] = useState(null);
 
-  const loadCreation = () => {
-    dataPortfolio.map(data => {
-       // Solucion 1
-      if (data.id === creationId) { // Solucion 2
-        setCreation(data);
-      }
-    })
-  }
-
   useEffect(() => {
-    loadCreation();
-  }, []);
+    const project = dataPortfolio.filter(elemento=>elemento.id == id)    
+
+    if (project.length >0)
+      setCreation(project[0]);    
+  }, [dataPortfolio,id]);
 
   if (creation) {
     return (
       <>
-        <div className="creationContainer">
-          <img src={creation.img} alt="" style={{margin: '20px' }} />
-          <div className="dataContainer">
-            <h1>{creation.description}</h1>
-            <h3 style={{width: '100%'}}>{creation.date}</h3>
-            {favorites.find((favorite) => favorite.id.toString() === creationId.toString()) ? (
-              <button onClick={() => deleteCreation(creation)}>Delete from favorites</button>
-            ):(
-              <button onClick={() => addCreation(creation)}>Add to favorites</button>
-            )}           
+        <section className="bg-bg_light_primary">
+      <div className="md:container px-5 pt-14">
+        <h2 className="title" data-aos="fade-down">
+          Detalle creación
+        </h2>
+        <h4 className="subtitle" data-aos="fade-down">
+          {creation.titulo}
+        </h4>
+        <br />
+        <div className="flex items-center md:flex-row flex-col-reverse ">
+          <img
+            src={creation.imagenes}
+            alt="..."
+            data-aos="fade-right"
+            className="max-w-sm md:block hidden"
+          />
+          <img
+            src={creation.imagenes}
+            data-aos="fade-up"
+            alt="..."
+            className="max-w-sm md:hidden"
+          />
+          <div
+            data-aos="fade-left"
+            className="border-2 border-dark_primary max-w-sm
+           p-6 shadow-sm rounded-xl rounded-br-[8rem] sm:min-w-[22rem]"
+          >
+            <p className="leading-7">{creation.id}</p>
+            <br />
+            <button className="btn bg-dark_primary text-white">
+              {creation.id}
+            </button>
           </div>
         </div>
+      </div>
+    </section>
       </>
     )
   } else {
     return (
-      <></>
+      <>holaaaa</>
     )
   }
 }
